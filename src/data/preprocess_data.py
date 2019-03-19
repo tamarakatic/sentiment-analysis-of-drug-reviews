4ba1b59d0b8560d031047035b1b5a6bf9eafa3d5
Tamara Katic <tamarakatic@gmail.com>
2019-03-19 23:38:10 +0100

    save cleaned data to csv file

--- src/data/preprocess_data.py
+++ src/data/preprocess_data.py
@@ -1,8 +1,18 @@
+import argparse
+
 import preprocessor
-from definitions import TEST_PROCESSED_PATH
+from definitions import TEST_PROCESSED_PATH, TRAIN_PROCESSED_PATH
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--train', action='store_true')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    preprocessor = preprocessor.Preprocessor(True)
-    preprocess_data = preprocessor.clean_data()
-    preprocess_data[['review', 'rating']].to_csv(TEST_PROCESSED_PATH, encoding='utf-8', index=False)
+    args = parse_args()
+    preprocess = preprocessor.Preprocessor(train=args.train)
+    preprocess_data = preprocess.clean_data()
+    path = TRAIN_PROCESSED_PATH if args.train else TEST_PROCESSED_PATH
+    preprocess_data.to_csv(path, encoding='utf-8', index=False)
