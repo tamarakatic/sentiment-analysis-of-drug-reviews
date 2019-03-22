4bd84e6f8488e32bebc3fc58bef84fa617b8abac
Tamara Katic <tamarakatic@gmail.com>
2019-03-22 13:43:05 +0100

    Try other ML methods

--- src/train_model.py
+++ src/train_model.py
@@ -1,66 +1,145 @@
 import pandas as pd
 from time import time
+from collections import defaultdict
 
-from sklearn.pipeline import Pipeline
+import pipelines
+from sklearn.externals import joblib
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn.feature_extraction.text import TfidfVectorizer
+from xgboost.sklearn import XGBClassifier
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
-from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import SGDClassifier
 from data.definitions import TRAIN_PROCESSED_PATH, TEST_PROCESSED_PATH
 
+joblib_model = "../models/finilized_model.pkl"
 
-pipeline = Pipeline([
-    ('tfidf', TfidfVectorizer()),
-    ('clf',  OneVsRestClassifier(LogisticRegression(random_state=0, solver='sag', max_iter=2000))),
-])
 
-parameters = {
-    'tfidf__use_idf': (True, False),
-}
+def bag_of_words_pipeline():
+    log_regression = pipelines.bag_of_words(
+        classifier=LogisticRegression(random_state=0,
+                                      solver='saga',
+                                      multi_class='ovr',
+                                      n_jobs=-1,
+                                      max_iter=2000)
+    )
+
+    log_regression_tfidf = pipelines.bag_of_words(
+        classifier=LogisticRegression(random_state=0,
+                                      solver='saga',
+                                      multi_class='ovr',
+                                      n_jobs=-1,
+                                      max_iter=2000),
+        tf_idf=True
+    )
+
+    multinomial_nb = pipelines.bag_of_words(
+        classifier=MultinomialNB()
+    )
+
+    multinomial_nb_tfidf = pipelines.bag_of_words(
+        classifier=MultinomialNB(),
+        tf_idf=True
+    )
+
+    linear_svc = pipelines.bag_of_words(
+        classifier=LinearSVC(multi_class='ovr', random_state=0, max_iter=2000)
+    )
+
+    linear_svc_tfidf = pipelines.bag_of_words(
+        classifier=LinearSVC(multi_class='ovr', random_state=0, max_iter=2000),
+        tf_idf=True
+    )
+
+    # tune: max_depth, min_child_weight, n_estimatorss
+    xgb = pipelines.bag_of_words(
+        classifier=XGBClassifier(
+            learning_rate=0.2, n_estimators=1000, max_depth=5, objective='multi:softprob',
+            n_class=3, n_jobs=-1, random_state=0)
+    )
+
+    xgb_tfidf = pipelines.bag_of_words(
+        classifier=XGBClassifier(
+            learning_rate=0.2, n_estimators=1000, max_depth=5, objective='multi:softprob',
+            n_class=3, n_jobs=-1, random_state=0),
+        tf_idf=True
+    )
+
+    sgd_classifier = pipelines.bag_of_words(
+        classifier=SGDClassifier(max_iter=2000, n_jobs=-1, random_state=0)
+    )
+
+    sgd_classifier_tfidf = pipelines.bag_of_words(
+        classifier=SGDClassifier(max_iter=2000, n_jobs=-1, random_state=0),
+        tf_idf=True
+    )
+
+    bow_pipelines = [
+        ("BoW + LR", log_regression),
+        ("BoW + LR + TFIDF", log_regression_tfidf),
+        ("BoW + MNB", multinomial_nb),
+        ("BoW + MNB + TFIDF", multinomial_nb_tfidf),
+        ("BoW + LinearSVC", linear_svc),
+        ("BoW + LinearSVC + TFIDF", linear_svc_tfidf),
+        ("BoW + XGBoost", xgb),
+        ("BoW + XGBoost + TFIDF", xgb_tfidf),
+        ("BoW + SGDClassifier", sgd_classifier),
+        ("BoW + SGDClassifier + TFIDF", sgd_classifier_tfidf)
+    ]
+
+    for name, model in bow_pipelines:
+        model.set_params(
+            vect__ngram_range=(1, 4)
+        )
+
+        yield (name, model)
+
 
 if __name__ == "__main__":
     preprocess_data = pd.read_csv(TRAIN_PROCESSED_PATH)
     preprocess_data.dropna(inplace=True)
     samples, labels = preprocess_data['review'], preprocess_data['rating']
 
+    test_df = pd.read_csv(TEST_PROCESSED_PATH)
+    test_df.dropna(inplace=True)
+
+    X_test, y_test = test_df['review'], test_df['rating']
+    target_names = ["Negative", "Neutral", "Positive"]
     X_train, X_val, y_train, y_val = train_test_split(
         samples, labels, test_size=0.20, random_state=42
     )
 
-    grid_search = GridSearchCV(pipeline,
-                               parameters,
-                               scoring="f1_macro",
-                               cv=5,
-                               verbose=1,
-                               n_jobs=-1)
+    results = defaultdict(list)
 
-    print("Performing Grid Search. . .")
-    print("Pipeline: {}".format(name for name, _ in pipeline.steps))
-    print("Parameters: {}".format(parameters))
+    for name, model in bag_of_words_pipeline():
+        print("\n\t".format(name))
 
-    start = time()
-    grid_search.fit(X_val, y_val)
+        results['model'].append(name)
+        start = time()
 
-    print("Training finished {:.3f}s".format(time() - start))
-    print("Best score is: {}".format(grid_search.best_score_))
-    print("Best parameters are:")
+        model.fit(X_train, y_train)
 
-    best_params = grid_search.best_estimator_.get_params()
-    for param_name in sorted(parameters.keys()):
-        print("\t{}: {}".format(param_name, best_params[param_name]))
+        end = time() - start
+        print("Training finished {:.3f}s".format(end))
+        results['time'].append(end)
 
-    pipeline.set_params(**best_params)
-    pipeline.fit(X_train, y_train)
+        print("Validation model\n")
+        prediction = model.predict(X_test)
 
-    print("Validation model\n")
-    test_df = pd.read_csv(TEST_PROCESSED_PATH)
-    test_df.dropna(inplace=True)
-    X_test, y_test = test_df['review'], test_df['rating']
+        for average in ['micro', 'macro', 'weighted']:
+            f1_score = metrics.f1_score(y_test, prediction, average=average)
+            results['f1_{}'.format(average)].append(f1_score)
 
-    prediction = grid_search.predict(X_test)
+        report = metrics.classification_report(y_test, prediction, target_names=target_names)
+        print("\n{}".format(report))
 
-    target_names = ["Negative", "Neutral", "Positive"]
-    report = metrics.classification_report(y_test, prediction, target_names=target_names)
-    print("\n{}".format(report))
+        with open("../reports/report.txt", "a+") as text_file:
+            print("Report for Model: {} is \n\t {}\n".format(name, report), file=text_file)
+
+        with open("../models/{}_model.pkl".format(name), 'wb') as fp:
+            joblib.dump(model, fp)
+
+    df = pd.DataFrame(results)
+    df = df[['model', 'f1_micro', 'f1_macro', 'f1_weighted', 'time']]
+    df.to_csv('../reports/results.csv', index=False, float_format='%.4f')
