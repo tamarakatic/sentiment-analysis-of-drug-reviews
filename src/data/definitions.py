3c79e03974b7eaef03626d6fd227de6accea30ab
Tamara Katic <tamarakatic@gmail.com>
2019-03-27 19:07:34 +0100

    Add path for bert model

--- src/data/definitions.py
+++ src/data/definitions.py
@@ -3,6 +3,17 @@ import os
 file_path = os.path.dirname(os.path.abspath(__file__))
 ROOT_PATH = os.path.dirname(os.path.dirname(file_path))
 
+DATA_BERT_PATH = os.path.join(ROOT_PATH, 'data/bert/')
+DATA_BERT = os.path.join(ROOT_PATH, 'data/bert/tmp')
+OUTPUT_BERT_DIR = os.path.join(ROOT_PATH, 'data/bert/tmp/class/output')
+
+BERT_PRETRAINED_PATH = '../models/pretrain/uncased_L-12_H-768_A-12/'
+PRETRAINED_BERT_CACHE = '../models/pretrain/uncased_L-12_H-768_A-12/cache/'
+
+DEV_BERT_PATH = os.path.join(ROOT_PATH, 'data/bert/dev.csv')
+TRAIN_BERT_PATH = os.path.join(ROOT_PATH, 'data/bert/train.csv')
+TEST_BERT_PATH = os.path.join(ROOT_PATH, 'data/bert/test.csv')
+
 TRAIN_RAW_PATH = os.path.join(ROOT_PATH, 'data/raw/drugs_train.tsv')
 TEST_RAW_PATH = os.path.join(ROOT_PATH, 'data/raw/drugs_test.tsv')
 
