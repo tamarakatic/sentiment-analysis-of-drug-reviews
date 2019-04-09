a57dae5d2c92a7824007c499bad5bb01e843dcb8
Tamara Katic <tamarakatic@gmail.com>
2019-04-09 17:34:19 +0200

    Evaluate bert mdoel

---
+++ src/eval_bert.py
@@ -0,0 +1,74 @@
+import os
+import pandas as pd
+from sklearn import metrics
+
+import torch
+
+from prepare_allennlp_data import dataset_reader
+from tl_allennlp.base_model import BaseModel
+from tl_allennlp.definitions import PRETRAINED_BERT
+from tl_allennlp.classifier_predictor import ClassifierPredictor
+
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.modules.seq2vec_encoders import Seq2VecEncoder
+from allennlp.modules.token_embedders.bert_token_embedder import PretrainedBertEmbedder
+from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
+from allennlp.data.iterators import BasicIterator
+
+HIDDEN_DIM = 128
+BATCH_SIZE = 32
+
+BERT_EMBEDDER = PretrainedBertEmbedder(
+    pretrained_model="bert-base-uncased",
+    top_layer_only=True
+)
+WORD_EMBEDDINGS = BasicTextFieldEmbedder({"tokens": BERT_EMBEDDER},
+                                         allow_unmatched_keys=True)
+
+
+class BertSentencePooler(Seq2VecEncoder):
+    def forward(self,
+                embs: torch.tensor,
+                mask: torch.tensor=None) -> torch.tensor:
+        return embs[:, 0]
+
+    def get_output_dim(self) -> int:
+        return WORD_EMBEDDINGS.get_output_dim()
+
+
+def load_bert_model():
+    vocab = Vocabulary()
+
+    encoder = BertSentencePooler(vocab)
+
+    model = BaseModel(word_embeddings=WORD_EMBEDDINGS,
+                      encoder=encoder,
+                      vocabulary=vocab)
+
+    output_bert_model_file = os.path.join(PRETRAINED_BERT, "bert_model.bin")
+    model.load_state_dict(torch.load(output_bert_model_file))
+    return model
+
+
+if __name__ == '__main__':
+    cuda_gpu = torch.cuda.is_available()
+    model = load_bert_model()
+
+    test_dataset = dataset_reader(train=False, elmo=False)
+
+    basic_iterator = BasicIterator(batch_size=BATCH_SIZE)
+    basic_iterator.index_with(model.vocab)
+
+    predictor = ClassifierPredictor(model, basic_iterator, cuda_device=0 if cuda_gpu else -1)
+    eval_results = predictor.evaluate(test_dataset)
+
+    y_true = [row['label'].array.argmax() for row in test_dataset]
+    y_pred = eval_results.argmax(axis=1).tolist()
+
+    f1_report = {}
+    for average in ['micro', 'macro', 'weighted']:
+        f1 = metrics.f1f1_score(y_pred, y_true, average=average)
+        f1_report['f1_{}'.format(average)] = f1
+
+    df = pd.DataFrame(f1_report)
+    df.to_csv('../reports/bert_report.csv', index=False)
