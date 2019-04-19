8c272c34b33fea76f0ecf09d72609a3682b86116
Tamara Katic <tamarakatic@gmail.com>
2019-04-19 13:54:47 +0200

    Try fast text with lstm

---
+++ src/train_fasttext_character.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+from data.definitions import DATA_PATH, PRETRAINED_FLAIR, FLAIR_LOSS, FLAIR_WEIGHTS
+
+from flair.data_fetcher import NLPTaskDataFetcher
+from flair.embeddings import WordEmbeddings, CharacterEmbeddings, DocumentRNNEmbeddings
+from flair.models import TextClassifier
+from flair.trainers import ModelTrainer
+from flair.visual.training_curves import Plotter
+
+
+def main():
+    train_dev_corpus = NLPTaskDataFetcher.load_classification_corpus(
+        Path(DATA_PATH),
+        train_file='flair_train.csv',
+        test_file='flair_test.csv',
+        dev_file='flair_dev.csv')
+
+    label_dict = train_dev_corpus.make_label_dictionary()
+
+    word_embeddings = [WordEmbeddings('glove'),
+                       CharacterEmbeddings()]
+
+    document_embeddings = DocumentRNNEmbeddings(word_embeddings,
+                                                rnn_type='LSTM',
+                                                hidden_size=128,
+                                                reproject_words=True,
+                                                reproject_words_dimension=64)
+
+    classifier = TextClassifier(document_embeddings,
+                                label_dictionary=label_dict,
+                                multi_label=False)
+
+    trainer = ModelTrainer(classifier, train_dev_corpus)
+    trainer.train(PRETRAINED_FLAIR,
+                  max_epochs=30,
+                  learning_rate=0.05,
+                  mini_batch_size=32,
+                  embeddings_in_memory=False,
+                  checkpoint=True)
+
+    plotter = Plotter()
+    plotter.plot_training_curves(FLAIR_LOSS)
+    plotter.plot_weights(FLAIR_WEIGHTS)
+
+
+if __name__ == "__main__":
+    main()
